153a41bb007dea080351846747258fa18eb9d4cd
edu1010 <39130123+edu1010@users.noreply.github.com>
2022-04-01 17:38:23 +0200

    thief

---
+++ Assets/ProjectBTs/BT_THIEF.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using BTs;
+
+[CreateAssetMenu(fileName = "BT_THIEF", menuName = "Behaviour Trees/BT_THIEF", order = 1)]
+public class BT_THIEF : BehaviourTree
+{
+    /* If necessary declare BT parameters here. 
+       All public parameters must be of type string. All public parameters must be
+       regarded as keys in/for the blackboard context.
+       Use prefix "key" for input parameters (information stored in the blackboard that must be retrieved)
+       use prefix "keyout" for output parameters (information that must be stored in the blackboard)
+
+       e.g.
+       public string keyDistance;
+       public string keyoutObject 
+     */
+
+     // construtor
+    public BT_THIEF()  { 
+        /* Receive BT parameters and set them. Remember all are of type string */
+    }
+    
+    public override void OnConstruction()
+    {
+        root = new Sequence(
+            new ACTION_Arrive("storeEntrance"),//llegar
+            new ACTION_Utter("x", "2"),//Gritar
+            new RepeatUntilSuccessDecorator(
+                new CONDITION_InstanceNear("2","police")
+                ),
+            new ACTION_Arrive("exitPoint")//irse detenido
+
+            );
+    }
+}
